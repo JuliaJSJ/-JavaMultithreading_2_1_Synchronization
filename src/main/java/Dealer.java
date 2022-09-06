abc30fdb2109cdf2d6c1efe850a553d96ab8f383
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-09-06 17:50:09 -0400

    Create Dealer.java

---
+++ src/main/java/Dealer.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealer {
+    private static final int RECIEVE_TIME = 2000;
+    private static final int SELL_TIME = 1000;
+    private static final int CARS = 10;
+    private final List<Car> cars = new ArrayList<>();
+
+    public void receiveCar() {
+        for (int i = 0; i < CARS; i++) {
+            try {
+                Thread.sleep(RECIEVE_TIME);
+                cars.add(new Car());
+                System.out.println(Thread.currentThread().getName() + " выпустил 1 автомобиль.");
+                synchronized (this) {
+                    notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void sellCar() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " зашел в автосалон.");
+            while (cars.size() == 0) {
+                System.out.println("нет доступных машин");
+                wait();
+            }
+            Thread.sleep(SELL_TIME);
+            System.out.println(Thread.currentThread().getName() + " уехал на новом Subaru.");
+            cars.remove(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
