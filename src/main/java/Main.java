c9d76cae2c311c4ba2139c5f0dc6e9f51d2fcb75
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-09-06 18:01:48 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    public static final int BUYERS = 4;
+    public static final int CUSTOMERS = 4;
 
     public static void main(String[] args) {
         final Dealer dealer = new Dealer();
-        for (int i = 1; i <= BUYERS; i++) {
+        for (int i = 1; i <= CUSTOMERS; i++) {
             new Thread(null, dealer::sellCar, "Покупатель " + i).start();
         }
-        new Thread(null, dealer::receiveCar, "Производитель Subaru").start();
+        new Thread(null, dealer::getCar, "Производитель Subaru").start();
     }
 }
