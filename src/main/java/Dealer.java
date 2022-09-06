94826a2299f01bce7481b9ae03486ff2036674c7
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-09-06 18:02:33 -0400

    Update Dealer.java

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dealer {
-    private static final int RECIEVE_TIME = 2000;
-    private static final int SELL_TIME = 1000;
     private static final int CARS = 10;
+    private static final int GET_TIME = 2000;
+    private static final int SELL_TIME = 1000;
+
+
     private final List<Car> cars = new ArrayList<>();
 
-    public void receiveCar() {
+    public void getCar() {
         for (int i = 0; i < CARS; i++) {
             try {
-                Thread.sleep(RECIEVE_TIME);
+                Thread.sleep(GET_TIME);
                 cars.add(new Car());
                 System.out.println(Thread.currentThread().getName() + " выпустил 1 автомобиль.");
                 synchronized (this) {
@@ -29,6 +31,7 @@ public class Dealer {
                 System.out.println("нет доступных машин");
                 wait();
             }
+
             Thread.sleep(SELL_TIME);
             System.out.println(Thread.currentThread().getName() + " уехал на новом Subaru.");
             cars.remove(0);
